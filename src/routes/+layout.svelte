2ca07a3f6a52209512e92795185f467d8a438a54
Vegard Kjeka Broen <vegarbro@ulrik.uio.no>
2026-04-05 01:22:03 +0200

    Add light theme and make default

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
-<script>
-  import '../app.css';
-  let { children } = $props();
+<script lang="ts">
+    import "../app.css";
+    import { onMount } from "svelte";
+    let { children } = $props();
+    onMount(async () => {
+        //const settings = await invoke<{ dark_mode: boolean }>(
+        //    "get_settings_frontend",
+        //);
+        const settings = { dark_mode: false };
+        document.documentElement.dataset.theme = settings.dark_mode
+            ? "dark"
+            : "light";
+    });
 </script>
 
 {@render children()}
